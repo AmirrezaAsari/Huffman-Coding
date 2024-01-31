a3a3e4cbcb87a1778536e371a5ba90cf7ecf96e8
unknown <amirreza.asari2@gmail.com>
2024-01-31 16:54:48 +0330

    tree data structure completed

--- tree.js
+++ tree.js
@@ -27,4 +27,86 @@ class BinaryTree{
             currentNode.right = new tree
         }
     }
+
+    search(data) {
+        let currentNode = this.root;
+        while(currentNode) {
+            if(data === currentNode.data){
+                return currentNode
+            }
+            currentNode = currentNode.left || currentNode.right;
+        }
+
+        return null;
+    }
+    delete(data) {
+        currentNode = this.root;
+        parentNode = null;
+    
+        while (currentNode) {
+          if (currentNode.data === data) {
+            // Found the node to delete
+            if (!currentNode.left && !currentNode.right) {
+              // Leaf node
+              if (!parentNode) {
+                // Root node
+                this.root = null;
+              } else {
+                if (currentNode === parentNode.left) {
+                  parentNode.left = null;
+                } else {
+                  parentNode.right = null;
+                }
+              }
+              break;
+            } else if (!currentNode.left || !currentNode.right) {
+              // Node with only one child
+              const childNode = currentNode.left ? currentNode.left : currentNode.right;
+    
+              if (!parentNode) {
+                // Root node
+                this.root = childNode;
+              } else {
+                if (currentNode === parentNode.left) {
+                  parentNode.left = childNode;
+                  childNode.parent = parentNode;
+                } else {
+                  parentNode.right = childNode;
+                  childNode.parent = parentNode;
+                }
+              }
+              break;
+            } else {
+              // Node with two children
+              const successor = currentNode.right;
+              let successorParent = currentNode;
+    
+              while (successor.left) {
+                successorParent = successor;
+                successor = successor.left;
+              }
+    
+              currentNode.data = successor.data;
+    
+              if (successorParent !== currentNode) {
+                successorParent.left = successor.right;
+                if (successor.right) {
+                  successor.right.parent = successorParent;
+                }
+              } else {
+                currentNode.right = successor.right;
+                if (successor.right) {
+                  successor.right.parent = currentNode;
+                }
+              }
+              break;
+            }
+          }
+    
+          parentNode = currentNode;
+          currentNode = currentNode.left || currentNode.right;
+        }
+      }
+
+
 }
