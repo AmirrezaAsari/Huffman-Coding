db5fb8b7c9658a043be94eb6c925dbf3dd700324
unknown <arya.khoshnava@gmail.com>
2024-02-04 00:52:51 +0330

    count Characters

--- huffmanCoding.js
+++ huffmanCoding.js
@@ -14,3 +14,20 @@ function readFile(filePath) {
 
 let filePath = "test.txt";
 console.log(readFile(filePath));
+
+function countCharacters(filePath) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const charCounts = {};
+
+    for (let i = 0; i < data.length; i++) {
+        const char = data[i];
+        if (!charCounts[char]) {
+            charCounts[char] = 0;
+        }
+        charCounts[char]++;
+    }
+
+    console.log(charCounts);
+}
+
+countCharacters('myFile.txt');
