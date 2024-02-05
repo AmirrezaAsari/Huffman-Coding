f9f19b5ff090dabc7ec1e51e6d89f3def1e659da
unknown <amirreza.asari2@gmail.com>
2024-02-05 21:39:44 +0330

    modified tree.js for huffman Tree

--- huffmanCoding.js
+++ huffmanCoding.js
@@ -1,19 +1,7 @@
 const fs = require("fs");
-
-function readFile(filePath) {
-    try {
-        const fileData = fs.readFileSync(filePath, "utf-8");
-        return fileData;
-    } catch (err) {
-        console.error('Error reading the file:', err);
-        throw err; // Re-throw the error to handle it outside the function
-    }
-}
+const HuffmanTree = require("./tree");
 
 
-
-let filePath = "test.txt";
-console.log(readFile(filePath));
 
 function countCharacters(filePath) {
     const data = fs.readFileSync(filePath, 'utf8');
@@ -26,8 +14,6 @@ function countCharacters(filePath) {
         }
         charCounts[char]++;
     }
-
-    console.log(charCounts);
+    
+    return charCounts;
 }
-
-countCharacters('myFile.txt');
